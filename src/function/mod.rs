8adb85612a83a566d8d5784d9c9a852ddca14c1d
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-04-19 12:17:21 +0700

    feat(function): add return and param

--- src/function/mod.rs
+++ src/function/mod.rs
@@ -4,4 +4,6 @@ mod simple;
 
 pub fn main() {
   simple::public_void();
+  simple::public_ret();
+  simple::public_param(1, 3);
 }
--- src/function/simple.rs
+++ src/function/simple.rs
@@ -1,3 +1,13 @@
 pub fn public_void() {
   println!("public_void: completed");
 }
+
+pub fn public_ret() -> i32 {
+  println!("public_ret: completed");
+  return 10;
+}
+
+pub fn public_param(a: i32, b: i32) -> i32 {
+  println!("public_param: completed");
+  return a + b;
+}
