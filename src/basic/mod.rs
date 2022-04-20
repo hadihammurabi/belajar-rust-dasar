f83998380cb907b82809c8fb590eebd677f8a511
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-04-20 21:16:57 +0700

    add design pattern

---
+++ src/basic/mod.rs
@@ -0,0 +1,24 @@
+// mod hello;
+// mod stdin;
+// mod mem;
+// mod structure;
+// mod collection;
+// mod errorhandling;
+// mod fs;
+// mod function;
+// mod generic;
+// mod test;
+// mod concurrent;
+
+pub fn main() {
+  // hello::main();
+  // stdin::main();
+  // mem::main();
+  // structure::main();
+  // collection::main();
+  // errorhandling::main();
+  // fs::main();
+  // function::main();
+  // generic::main();
+  // concurrent::main();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,5 @@
-// mod hello;
-// mod stdin;
-// mod mem;
-// mod structure;
-// mod collection;
-// mod errorhandling;
-// mod fs;
-// mod function;
-// mod generic;
-// mod test;
-// mod concurrent;
+// mod basic;
 
 fn main() {
-    // hello::main();
-    // stdin::main();
-    // mem::main();
-    // structure::main();
-    // collection::main();
-    // errorhandling::main();
-    // fs::main();
-    // function::main();
-    // generic::main();
-    // concurrent::main();
+    // basic::main();
 }
