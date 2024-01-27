4769b4de13479b663235e3072c3beee121b0f0f9
hadihammurabi <hadihammurabi@gmail.com>
2024-01-28 06:36:45 +0700

    feat(reference): borrow then mutate

--- src/basic/mem/reference.rs
+++ src/basic/mem/reference.rs
@@ -2,9 +2,19 @@ pub fn reference() {
   let x = String::from("hello");
   let y = &x;
   reference_print(&x, y);
+
+  let mut s = String::from("mut this");
+
+  {
+    let a = &s;
+    let b = &s;
+    reference_print(a, b);
+  }
+
+  s = String::from("mutated");
+  reference_print(&s, &s);
 }
 
 fn reference_print(x: &String, y: &String) {
-  println!("x: {} & y: {}", x, y);
+  println!("x: {x}, y: {y}");
 }
-
